b0022e94e26eae0b0e35cb4407e138c60cfa118b
dominhdang148 <myclone235@gmail.com>
2024-02-04 01:02:49 +0700

    Add sliders, finished chap_8

--- App/Screens/HomeScreen/Slider.jsx
+++ App/Screens/HomeScreen/Slider.jsx
@@ -1,22 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import GlobalApi from '../../../Utils/GlobalApi';
+import React, { useEffect, useState } from "react";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import GlobalApi from "../../../Utils/GlobalApi";
 
 export default function Slider() {
-    const [slider, setSliders] = useState();
+    const [sliders, setSliders] = useState();
     useEffect(() => {
         getSliders();
-    }, [])
+    }, []);
     const getSliders = () => {
         GlobalApi.getSliders().then((resp) => {
-            console.log("resp: ", resp.sliders)
-            setSlider(resp?.sliders) 
-        })
-    }
-
+            console.log("resp: ", resp.sliders);
+            setSliders(resp?.sliders);
+        });
+    };
     return (
         <View>
-            <Text>Offer For You</Text>
+            <Text style={styles.heading}>Offer For You</Text>
+            <FlatList
+                data={sliders}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item, index }) => (
+                    <View style={{ marginRight: 20 }}>
+                        <Image
+                            source={{ uri: item?.image?.url }}
+                            style={styles.sliderImage}
+                        />
+                    </View>
+                )}
+            />
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    heading: {
+        fontSize: 20,
+        fontFamily: "outfit-medium",
+        marginBottom: 10,
+    },
+    sliderImage: {
+        width: 270,
+        height: 150,
+        borderRadius: 20,
+        objectFit: "contain",
+    },
+});
